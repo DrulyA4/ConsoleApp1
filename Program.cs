434ce4a4ff40b8c7fbc89ac8f07d7376d886b2c4
Druly <Druly@DESKTOP-F8QQ64O>
2024-10-14 19:14:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число 1 ");
+            var num = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число 2 ");
+            var num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число 3 ");
+            var num3 = Convert.ToDouble(Console.ReadLine());
+
+            var res = num * 2;
+            var res2 = num2 - 3;
+            var res3 = Math.Pow(num3, 2);
+            var sum = res + res2 + res3;
+            Console.WriteLine($" {res} {res2} {res3} {sum}"); 
+        }
+    }
+}
+
